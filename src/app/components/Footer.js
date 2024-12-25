49812c1acecdcc144f44740493ca6ebd79a07e18
Philip419ik <p.ikani@rgu.ac.uk>
2024-12-25 17:45:53 +0100

    Footer edited

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -9,16 +9,16 @@ const Footer = () => {
       className="text-center py-4"
     >
       <p>
-        Made with love by{" "}
+        Developed by Group 10; Read our Documentation{" "}
         <a
           href="https://docs.google.com/document/d/1g9iv1Fy13sT9EWjfchvsM5QA92fUebaamIh8-4yE11E/edit?usp=sharing" // Replace with your actual Google Docs link
           target="_blank"
           rel="noopener noreferrer"
           className="text-blue-600 underline"
         >
-          Group 10; 
+          Here
         </a>
-         All rights reserved.
+        .
       </p>
     </footer>
   );
